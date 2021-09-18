87a44213e039255f8d1d1153c81b0eefa48ecaee
coffeetea99 <coffeetea99iro@gmail.com>
2021-09-18 14:20:49 +0900

    install & apply cors
    commands used:
          npm install cors
          npm install -D @types/cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 app.set('port', process.env.PORT || 3009);
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
