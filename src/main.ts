9371405910989aeec450ad3bb4a8a8413e9c3be7
Jinje Han <coffeetea99iro@gmail.com>
2023-06-10 22:47:53 +0900

    install class-validator & swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import config from './config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('coffeetea-backend')
+    .setDescription('coffeetea-backend API description')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api', app, swaggerDocument);
+
   await app.listen(config.SERVER_PORT);
 }
 bootstrap();
